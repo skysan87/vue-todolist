c1064f7d722d62398e68d2100595531a90d4e55e
skysan87 <skysan.justacase@gmail.com>
2019-10-03 07:16:10 +0900

    build

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "./registerServiceWorker";
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
