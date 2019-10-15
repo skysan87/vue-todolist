39d08eec94e052f7da52e92da00af560804886c8
skysan87 <skysan.justacase@gmail.com>
2019-10-15 23:46:42 +0900

    uninstall bootstrap

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import "./registerServiceWorker";
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import BootstrapVue from 'bootstrap-vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEllipsisV, faEdit } from '@fortawesome/free-solid-svg-icons'
@@ -14,7 +11,6 @@ library.add(faEllipsisV, faEdit)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
